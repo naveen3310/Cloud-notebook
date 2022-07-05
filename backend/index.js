99ddc24603c4fb4e54d32e651f2d380132ea2cc5
Naveen_Kumar <ankitmahich00786@gmail.com>
2022-07-05 10:44:22 +0530

    new changes after concurrently both adding

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 
 connectMongo();
-const PORT = 3000;
+const PORT = 5000;
 //for passing data between req and res.
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/notes", required("./routes/notes"));
+app.use("/api/notes", require("./routes/notes"));
 
 // app.get("/", (req, res) => {
 //   res.send("HII");
